c7a2530e23e2f35beddd0e96fd20404360653095
Muhammad Taha zafar <tahazafar112@gmail.com>
2025-04-08 21:10:56 +0500

    4-8-2025

--- lib/forcontroller_MVVM/myController.dart
+++ lib/forcontroller_MVVM/myController.dart
@@ -2,11 +2,12 @@ import 'package:get/get.dart';
 
 class MyController extends GetxController {
   //var count = 0.obs;
-  var count =0;
+  var count = 0;
 
   incrementcount() {
     count ++;
     update(['123']);
   }
 
+  //asd
 }
